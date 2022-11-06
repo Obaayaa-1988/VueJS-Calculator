da8cf7dc3d4fe9cdda8741ba9b3431908552fe9f
Obaayaa-1988 <aquaicoe31@gmail.com>
2022-11-06 19:27:07 +0000

    appending string to current datat implemented

--- src/components/CalculatorApp.vue
+++ src/components/CalculatorApp.vue
@@ -8,19 +8,19 @@
       <div @click="sign" class="btn">+/-</div>
       <div @click="clear" class="btn">C</div>
       <div class="btn color">+</div>
-      <div class="btn">7</div>
-      <div class="btn">8</div>
-      <div class="btn">9</div>
-      <div class="btn color">x</div>
-      <div class="btn">4</div>
-      <div class="btn">5</div>
-      <div class="btn">6</div>
+      <div @click="append('9')" class="btn">9</div>
+      <div @click="append('8')" class="btn">8</div>
+      <div @click="append('7')" class="btn">7</div>
+      <div class="btn color">x</div> 
+      <div @click="append('6')" class="btn">6</div>
+      <div @click="append('5')" class="btn">5</div>
+      <div @click="append('4')" class="btn">4</div>
       <div class="btn color">-</div>
-      <div class="btn">1</div>
-      <div class="btn">2</div>
-      <div class="btn">3</div>
+      <div @click="append('1')" class="btn">1</div>
+      <div @click="append('2')" class="btn">2</div>
+      <div @click="append('3')" class="btn">3</div>
       <div class="btn color">/</div>
-      <div class="btn zero">0</div>
+      <div @click="append('0')" class="btn zero">0</div>
       <div class="btn">.</div>
       <div class="btn color">=</div>
     </div>
@@ -36,7 +36,7 @@ export default {
   },
   data() {
     return {
-      current: "222",
+      current: "",
     };
   },
 
@@ -53,7 +53,8 @@ export default {
 
     //appending the string current to the numbers
 
-    append(){
+    append(number){
+      this.current = this.current + number
 
     },
 
@@ -115,4 +116,6 @@ export default {
   background-color: tomato;
   color: white;
 }
+
+
 </style>
